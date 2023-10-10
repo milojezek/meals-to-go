73995505a66c2a810956a1496321d1a4d0e335ea
Miloslav Jezek <miloslav.jezek@fpt.sk>
2023-10-10 13:19:38 +0200

    Implement eslint and prettier

--- App.js
+++ App.js
@@ -1,27 +1,21 @@
-import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
-import { 
-  StatusBar,
-  StyleSheet,
-  Text,
-  View,
-  SafeAreaView,
-  Platform 
-} from 'react-native';
+import React from "react";
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import { StatusBar, StyleSheet, Text, View, SafeAreaView } from "react-native";
 
 /**
  * SafeAreaView works on iOS only!
  * On Android we need to know the height of status bar.
  * StatusBar.currentHeight works for Android only.
- * @returns 
+ * @returns
  */
 export default function App() {
   return (
     <>
       <SafeAreaView style={styles.container}>
-        <View style={{ backgroundColor: "green", padding: 15, marginTop: StatusBar.currentHeight }}>
+        <View style={styles.search}>
           <Text style={styles.text}>Search</Text>
         </View>
-        <View style={{ flex: 1, backgroundColor: "blue", padding: 15 }}>
+        <View style={styles.list}>
           <Text>list</Text>
         </View>
       </SafeAreaView>
@@ -34,5 +28,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: "column",
+  },
+  search: {
+    backgroundColor: "green",
+    padding: 15,
+    marginTop: StatusBar.currentHeight,
+  },
+  list: {
+    flex: 1,
+    backgroundColor: "blue",
+    padding: 15,
   },
 });
