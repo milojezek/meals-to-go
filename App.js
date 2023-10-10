5842ee182918113df6e7ae0de2bead17a3524c4d
Miloslav Jezek <miloslav.jezek@fpt.sk>
2023-10-10 12:39:43 +0200

    Solve flex exercise; Resolve Android status bar issue

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
+import { 
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Platform 
+} from 'react-native';
 
+/**
+ * SafeAreaView works on iOS only!
+ * On Android we need to know the height of status bar.
+ * StatusBar.currentHeight works for Android only.
+ * @returns 
+ */
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeAreaView style={styles.container}>
+        <View style={{ backgroundColor: "green", padding: 15, marginTop: StatusBar.currentHeight }}>
+          <Text style={styles.text}>Search</Text>
+        </View>
+        <View style={{ flex: 1, backgroundColor: "blue", padding: 15 }}>
+          <Text>list</Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style="auto" />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "column",
   },
 });
